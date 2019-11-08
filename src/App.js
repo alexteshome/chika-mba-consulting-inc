12fd6a88daa45567d70b1fbeeb0735c2ee1bd3b0
Alex Teshome <alex_teshome@hotmail.com>
2019-11-08 16:33:22 -0500

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -10,14 +10,10 @@ function App() {
   return (
     <div>
       <ResponsiveContainer>
-        <Route
-          path={process.env.PUBLIC_URL + "/"}
-          exact
-          render={() => <Redirect to="/home" />}
-        />
-        <Route path={process.env.PUBLIC_URL + "/home"} component={Home} />
-        <Route path={process.env.PUBLIC_URL + "/about"} component={About} />
-        <Route path={process.env.PUBLIC_URL + "/contact"} component={Contact} />
+        <Route path="/" exact render={() => <Redirect to="/home" />} />
+        <Route path="/home" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
       </ResponsiveContainer>
       <Footer />
     </div>
